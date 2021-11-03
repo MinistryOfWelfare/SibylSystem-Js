638c4770eb68869c347a6aad367fa46166212bc1
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-11-03 19:33:43 +0530

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,6 @@
+const { PyschoPass }  =  require("../index.js");
+
+const Sibyl  =  new  PyschoPass({token: "1915921298:lHN6O6fpbWiXrqLvlEHTDWrNxfZDtZfVBf7DC_DQZ4bCsrw_R1jVzUXAQc3nRFTR"});
+
+//    SibylSystem wrapper js Copyright (C) 2021 MoeZilla
+Sibyl.getInfo("1610284626").then(console.log)
